4ca2fdea489fed1a3e80e58538dd2e14f4152da7
AnanevSYU <ananevsyu@vip.cbr.ru>
2024-02-04 11:54:39 +0300

    Класс SkipIterator

--- 0.0 MY_PYTHON_TEST.py
+++ 0.0 MY_PYTHON_TEST.py
@@ -1,21 +1,4 @@
-class Counter:
-    def __init__(self, low, high):
-        self.low = low
-        self.high = high
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    def __iter__(self):
-        return self
 
-    def __next__(self):
-        if self.low > self.high:
-            raise StopIteration
-        self.low += 1
-        return self.low-1
-        
-        
-#####################################################        
-        
-counter1 = Counter(3, 10)               # создаем итератор Counter, передавая значения low=3, high=10
-
-for i in counter1:                      # неявно вызываем функцию next()
-    print(i)        
+print(a[::3])
---
+++ 6.1.4.py
@@ -0,0 +1,84 @@
+'''
+Класс SkipIterator
+Реализуйте класс SkipIterator. При создании экземпляра класс должен принимать два аргумента в следующем порядке:
+
+iterable — итерируемый объект
+n — целое неотрицательное число
+Экземпляр класса SkipIterator должен являться итератором, который генерирует элементы итерируемого объекта iterable, пропуская по n элементов, а затем возбуждает исключение StopIteration.
+
+Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+
+Примечание 2. Класс SkipIterator должен удовлетворять протоколу итератора, то есть иметь методы __iter__() и __next__(). Реализация же протокола может быть произвольной.
+'''
+
+class SkipIterator:
+    def __init__(self,iterable,n):
+        self.iterable = iter(list(iterable)[::n+1])
+        
+    def __iter__(self):
+        return self
+        
+    def __next__(self):
+        return next(self.iterable)
+            
+        
+        
+# INPUT DATA:
+
+# TEST_1:
+skipiterator = SkipIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)   # пропускаем по одному элементу
+
+print(*skipiterator)
+
+# TEST_2:
+skipiterator = SkipIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)   # пропускаем по два элемента
+
+print(*skipiterator)
+
+# TEST_3:
+skipiterator = SkipIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)   # не пропускаем элементы
+
+print(*skipiterator)
+
+# TEST_4:
+skipiterator = SkipIterator('abcd', 0)
+
+print(*skipiterator)
+
+# TEST_5:
+skipiterator = SkipIterator(['abcd'], 1)
+
+print(*skipiterator)
+
+# TEST_6:
+skipiterator = SkipIterator('abcd', 3)
+
+print(*skipiterator)
+
+# TEST_7:
+skipiterator = SkipIterator(iter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 5)
+
+print(*skipiterator)
+
+# TEST_8:
+skipiterator = SkipIterator(iter(['aa', 'bb', 'cc', 'dd', 'ee', 'ff']), 2)
+
+print(*skipiterator)
+
+# TEST_9:
+data = ['к', 'б', 'ш', 'к', 'к', 'о', 'т', 'г', 'о', 'д', 'р', 'в', 'с', 'с', 'и', 'о', 'в', 'п', 'у', 'с', 'л', 'т',
+        'г', 'т', 'з', 'ь', 'о', 'п', 'н', 'в', 'и', 'н', 'с', 'п', 'р', 'ш', 'е', 'к', 'н', 'с', 'у', 'в', 'п', 'т',
+        'х', 'т', 'с', 'с', 'л', 'с']
+skipiterator = SkipIterator(iter(data), 4)
+
+print(*skipiterator)
+
+# TEST_10:
+skipiterator = SkipIterator(range(1000), 7)
+
+for _ in range(25):
+    next(skipiterator)
+
+print(next(skipiterator))
+print(next(skipiterator))
+print(next(skipiterator))
